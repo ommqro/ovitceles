6b5c46a3f46c15e55bf8ac5cd3a470f17de3ed35
mautematico <mauricio@navarromiranda.mx>
2015-03-16 00:40:29 -0600

    Elijo una versión de cada uno de los quince problemas.

--- js/problemas.js
+++ js/problemas.js
@@ -9,26 +9,6 @@ Problemas = [{
         "E": "$20$"
     }
 }, {
-    "id": "1-b",
-    "enunciado": "Un sapo tarda 8 segundos para dar 4 saltos continuos. Para dar 15 saltos continuos tarda, en segundos:",
-    "opciones": {
-        "A": "$30$",
-        "B": "$12$",
-        "C": "$15$",
-        "D": "$18$",
-        "E": "$20$"
-    }
-}, {
-    "id": "2-a",
-    "enunciado": "Dada la sucesión de letras de la palabra olimpiada: \\[olimpiadaolimpiadaolimpiadaolimpiadaolimpiada...\\] La letra que ocupa la posición 2015 es",
-    "opciones": {
-        "A": "$a$",
-        "B": "$o$",
-        "C": "$d$",
-        "D": "$i$",
-        "E": "$m$"
-    }
-}, {
     "id": "2-b",
     "enunciado": "Dada la sucesión de letras de la palabra matematicas: \\[matematicasmatematicasmatematicasmatematicasmatematicas...\\] La letra que ocupa la posición 2015 es ",
     "opciones": {
@@ -49,16 +29,6 @@ Problemas = [{
         "E": "$2010$"
     }
 }, {
-    "id": "3-b",
-    "enunciado": "De los siguientes números el que es divisible por la suma de sus dígitos es",
-    "opciones": {
-        "A": "$2015$",
-        "B": "$2016$",
-        "C": "$2014$",
-        "D": "$2012$",
-        "E": "$2013$"
-    }
-}, {
     "id": "4-a",
     "enunciado": "Chuy elige un número de dos cifras, se lo resta a 300 y el número obtenido lo multiplica por 3; el resultado que no puede obtener es:",
     "opciones": {
@@ -67,26 +37,6 @@ Problemas = [{
         "C": "$630$",
         "D": "$750$",
         "E": "$840$"
-    }
-}, {
-    "id": "4-b",
-    "enunciado": "Toñito elige un número de dos cifras, se lo resta a 200 y el número obtenido lo multiplica por 2; el resultado que no puede obtener es:",
-    "opciones": {
-        "A": "$204$",
-        "B": "$362$",
-        "C": "$382$",
-        "D": "$256$",
-        "E": "$312$"
-    }
-}, {
-    "id": "5-a",
-    "enunciado": "Si $A$ es el conjunto de los números de 1 a 100 cuyo menor divisor primo es 5, la cantidad de elementos de A es:",
-    "opciones": {
-        "A": "$15$",
-        "B": "$10$",
-        "C": "$7$",
-        "D": "$6$",
-        "E": "$5$"
     }
 }, {
     "id": "5-b",
@@ -109,26 +59,6 @@ Problemas = [{
         "E": "$9$"
     }
 }, {
-    "id": "6-b",
-    "enunciado": "Los enteros positivos 24,18 y M tienen la propiedad de que el producto de cualquiera dos de ellos es múltiplo del tercero. El menor valor posible para M es: ",
-    "opciones": {
-        "A": "$3$",
-        "B": "$12$",
-        "C": "$6$",
-        "D": "$18$",
-        "E": "$9$"
-    }
-}, {
-    "id": "7-a",
-    "enunciado": "Juan durmió el lunes $3\\frac{1}{2}$ horas; el martes, $1\\frac{1}{3}$ horas; el miércoles, $1\\frac{1}{2}$ y el jueves, $3\\frac{1}{3}$ . Cuántas horas durmió en total durante los cuatro días?.",
-    "opciones": {
-        "A": "$8\\frac{1}{8}$",
-        "B": "$8\\frac{2}{6}$",
-        "C": "$\\frac{51}{6}$",
-        "D": "$9\\frac{2}{3}$",
-        "E": "$8\\frac{5}{6}$"
-    }
-}, {
     "id": "7-b",
     "enunciado": "Mauricio durmió el lunes $1\\frac{1}{2}$ horas ; el martes, $1\\frac{1}{3}$ horas; el miércoles, $3\\frac{1}{2}$  y el jueves, $3\\frac{1}{3}$. Cuántas horas durmió en total durante los cuatro días?.",
     "opciones": {
@@ -147,26 +77,6 @@ Problemas = [{
         "C": "$25.791$",
         "D": "$26.911$",
         "E": "$26.791$"
-    }
-}, {
-    "id": "8-b",
-    "enunciado": "¿Qué número restado a 394.801 da como resultado 367.89?",
-    "opciones": {
-        "A": "$23.791$",
-        "B": "$24.791$",
-        "C": "$25.791$",
-        "D": "$26.911$",
-        "E": "$26.791$"
-    }
-}, {
-    "id": "9-a",
-    "enunciado": "¿Cuál es el número que después de multiplicarlo por $5$, restarle $2$ y sumarle $3$ da como resultado  $16$ ?",
-    "opciones" : {
-        "A": "$8$",
-        "B": "$1$",
-        "C": "$5$",
-        "D": "$2$",
-        "E": "$3$"
     }
 }, {
     "id": "9-b",
@@ -189,16 +99,6 @@ Problemas = [{
         "E": "$Ninguna$"
     }
 }, {
-    "id": "10-b",
-    "enunciado": "Si un padre tiene $35$ años y su hija $9$, ¿dentro de cuántos años de edad del padre será el triple que la de la hija?.",
-    "opciones": {
-        "A": "$9$",
-        "B": "$4$",
-        "C": "$3$",
-        "D": "$5$",
-        "E": "$Ninguna$"
-    }
-}, {
     "id": "11-a",
     "enunciado": "Rosario tiene $7$ cajas de chocolates y $5$ sueltos.Si las cajas contienen el mismo número de chocolates y en total son $96$, ¿cuántos chocolates hay en cada caja ? ",
     "opciones" : {
@@ -207,26 +107,6 @@ Problemas = [{
         "C": "$11$",
         "D": "$12$",
         "E": "$13$"
-    }
-}, {
-    "id": "11-b",
-    "enunciado": "Rosario tiene $6$ cajas de chocolates y $9$ sueltos.Si las cajas contienen el mismo número de chocolates y en total son $63$, ¿cuántos chocolates hay en cada caja ?",
-    "opciones" : {
-        "A": "$9$",
-        "B": "$10$",
-        "C": "$11$",
-        "D": "$12$",
-        "E": "$13$"
-    }
-}, {
-    "id": "12-a",
-    "enunciado": "Se tienen ocho monedas aparentemente idénticas, de las cuales una es falsa y su característica es que es más delgada y por lo tanto de menor peso.¿Cuál es el mínimo número de pesadas que puedes hacer para encontrar la moneda falsa?",
-    "opciones" : {
-        "A": "$1$",
-        "B": "$2$",
-        "C": "$3$",
-        "D": "$4$",
-        "E": "$5$"
     }
 }, {
     "id": "12-b",
@@ -239,16 +119,6 @@ Problemas = [{
         "E": "$5$"
     }
 }, {
-    "id": "13-a",
-    "enunciado": "¿Cuántos números distintos de cuatro cifras se pueden formar con unos y ceros?",
-    "opciones": {
-        "A": "$6$",
-        "B": "$8$",
-        "C": "$10$",
-        "D": "$12$",
-        "E": "$14$"
-    }
-}, {
     "id": "13-b",
     "enunciado": "¿Cuántos números distintos de cuatro cifras se pueden formar con los dígitos dos y cinco ?",
     "opciones" : {
@@ -257,16 +127,6 @@ Problemas = [{
         "C": "$10$",
         "D": "$12$",
         "E": "$14$"
-    }
-}, {
-    "id": "14-a",
-    "enunciado": "Una persona tiene tres monedas de distintos valores.¿Cuántas cantidades diferentes de dinero puede formar con las monedas?",
-    "opciones" : {
-        "A": "$3$",
-        "B": "$4$",
-        "C": "$5$",
-        "D": "$6$",
-        "E": "$7$"
     }
 }, {
     "id": "14-b",
@@ -278,17 +138,6 @@ Problemas = [{
         "D": "$4$",
         "E": "$3$"
     }
-}, {
-    "id": "15-a",
-    "enunciado": "Víctor tiene $9$ cuadrados iguales y con ellos forma un cuadrado más grande, como se muestra en la figura. Si el área del cuadrado grande es  $81cm^{2}$, ¿cuánto mide el área de cada uno de los cuadrados pequeños?",
-    "opciones": {
-        "A": "$0$",
-        "B": "$3$",
-        "C": "$6$",
-        "D": "$9$",
-        "E": "Ninguna de las anteriores"
-    },
-    "imagen": "img/cuadros.jpg"
 }, {
     "id": "15-b",
     "enunciado": "Víctor tiene $9$ cuadrados iguales y con ellos forma un cuadrado más grande, como se muestra en la figura. Si el perímetro del cuadrado grande es $36cm^{2}$, ¿cuánto mide el área de cada uno de los cuadrados pequeños?",
