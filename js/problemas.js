e2d757bffff187637a63eda80afe649e67bdce34
mautematico <mauricio@navarromiranda.mx>
2015-05-08 14:03:08 -0500

    Nuevos problemas (mayo 2015

--- js/problemas.js
+++ js/problemas.js
@@ -1,152 +1,318 @@
-Problemas = [{
-    "id": "1-a",
-    "enunciado": "Un sapo tarda 6 segundos para dar 3 saltos continuos. Para dar 10 saltos continuos tarda, en segundos:",
-    "opciones": {
-        "A": "$10$",
-        "B": "$12$",
-        "C": "$15$",
-        "D": "$18$",
-        "E": "$20$"
-    }
-}, {
-    "id": "2-b",
-    "enunciado": "Dada la sucesión de letras de la palabra matematicas: \\[matematicasmatematicasmatematicasmatematicasmatematicas...\\] La letra que ocupa la posición 2015 es ",
-    "opciones": {
-        "A": "$a$",
-        "B": "$m$",
-        "C": "$e$",
-        "D": "$t$",
-        "E": "$i$"
-    }
-}, {
-    "id": "3-a",
-    "enunciado": "De los siguientes números el que es divisible por la suma de sus dígitos es",
-    "opciones": {
-        "A": "$2012$",
-        "B": "$2009$",
-        "C": "$2011$",
-        "D": "$2008$",
-        "E": "$2010$"
-    }
-}, {
-    "id": "4-a",
-    "enunciado": "Chuy elige un número de dos cifras, se lo resta a 300 y el número obtenido lo multiplica por 3; el resultado que no puede obtener es:",
-    "opciones": {
-        "A": "$570$",
-        "B": "$606$",
-        "C": "$630$",
-        "D": "$750$",
-        "E": "$840$"
-    }
-}, {
-    "id": "5-b",
-    "enunciado": "Si $A$ es el conjunto de los números de 1 a 100 cuyo menor divisor primo es 7, la cantidad de elementos de $A$ es:",
-    "opciones": {
-        "A": "$5$",
-        "B": "$4$",
-        "C": "$3$",
-        "D": "$2$",
-        "E": "$1$"
-    }
-}, {
-    "id": "6-a",
-    "enunciado": "Los enteros positivos 24,36 y N tienen la propiedad de que el producto de cualquiera dos de ellos es múltiplo del tercero. El menor valor posible para N es: ",
-    "opciones": {
-        "A": "$3$",
-        "B": "$12$",
-        "C": "$6$",
-        "D": "$18$",
-        "E": "$9$"
-    }
-}, {
-    "id": "7-b",
-    "enunciado": "Mauricio durmió el lunes $1\\frac{1}{2}$ horas ; el martes, $1\\frac{1}{3}$ horas; el miércoles, $3\\frac{1}{2}$  y el jueves, $3\\frac{1}{3}$. Cuántas horas durmió en total durante los cuatro días?.",
-    "opciones": {
-        "A": "$8\\frac{1}{8}$",
-        "B": "$8\\frac{2}{6}$",
-        "C": "$\\frac{51}{6}$",
-        "D": "$9\\frac{2}{3}$",
-        "E": "$8\\frac{5}{6}$"
-    }
-}, {
-    "id": "8-a",
-    "enunciado": "¿Qué número restado a 345.58 da como resultado 320.789?",
-    "opciones": {
-        "A": "$23.791$",
-        "B": "$24.791$",
-        "C": "$25.791$",
-        "D": "$26.911$",
-        "E": "$26.791$"
-    }
-}, {
-    "id": "9-b",
-    "enunciado" : "¿Cuál es el número que después de multiplicarlo por $3$, restarle $6$ y sumarle $3$ da como resultado $15$ ?",
-    "opciones" : {
-        "A": "$8$",
-        "B": "$6$",
-        "C": "$5$",
-        "D": "$2$",
-        "E": "$3$"
-    }
-}, {
-    "id": "10-a",
-    "enunciado": "Si un padre tiene $34$ años y su hija $7$, ¿dentro de cuántos años, la edad del padre será el triple que la de la hija?.",
-    "opciones": {
-        "A": "$9$",
-        "B": "$10$",
-        "C": "$3$",
-        "D": "$5$",
-        "E": "$Ninguna$"
-    }
-}, {
-    "id": "11-a",
-    "enunciado": "Rosario tiene $7$ cajas de chocolates y $5$ sueltos.Si las cajas contienen el mismo número de chocolates y en total son $96$, ¿cuántos chocolates hay en cada caja ? ",
-    "opciones" : {
-        "A": "$9$",
-        "B": "$10$",
-        "C": "$11$",
-        "D": "$12$",
-        "E": "$13$"
-    }
-}, {
-    "id": "12-b",
-    "enunciado": "Se tienen ocho monedas aparentemente idénticas, de las cuales una es falsa y su característica es que es más delgada y por lo tanto de menor peso.¿Cuál es el mínimo número de pesadas que puedes hacer para encontrar la moneda falsa?",
-    "opciones" : {
-        "A": "$1$",
-        "B": "$2$",
-        "C": "$3$",
-        "D": "$4$",
-        "E": "$5$"
-    }
-}, {
-    "id": "13-b",
-    "enunciado": "¿Cuántos números distintos de cuatro cifras se pueden formar con los dígitos dos y cinco ?",
-    "opciones" : {
-        "A": "$16$",
-        "B": "$8$",
-        "C": "$10$",
-        "D": "$12$",
-        "E": "$14$"
-    }
-}, {
-    "id": "14-b",
-    "enunciado": "Jorge tiene tres monedas de distintos valores.¿Cuántas sumas diferentes de dinero puede formar con las monedas?",
-    "opciones" : {
-        "A": "$7$",
-        "B": "$6$",
-        "C": "$5$",
-        "D": "$4$",
-        "E": "$3$"
-    }
-}, {
-    "id": "15-b",
-    "enunciado": "Víctor tiene $9$ cuadrados iguales y con ellos forma un cuadrado más grande, como se muestra en la figura. Si el área del cuadrado grande es $36cm^{2}$, ¿cuánto mide el área de cada uno de los cuadrados pequeños?",
-    "opciones" : {
-        "A": "$4$",
-        "B": "$3$",
-        "C": "$6$",
-        "D": "$9$",
-        "E": "Ninguna de las anteriores"
+Problemas = [
+    {
+        "id":"1-a",
+        "enunciado":" ¿Cuál número es mayor que -2/3 y menor que 1.29? ",
+        "opciones":{
+            "A":"$-1.25$",
+            "B":"$-0.75$",
+            "C":"$-\\frac{4}{5}$",
+            "D":"$\\frac{5}{4}$",
+            "E":"$\\frac{3}{2}$"
+        }
     },
-    "imagen": "img/cuadros.jpg"
-}];
+    {
+        "id":"2-b",
+        "enunciado":" Dados los ángulos marcados en la figura, ¿cuál es el valor de $\\alpha + \\beta - \\theta - \\delta$ ?",
+        "opciones":{
+            "A":"$90°$",
+            "B":"$60°$",
+            "C":"$30°$",
+            "D":"$0°$",
+            "E":"No se puede contestar"
+        },
+        "imagen":"img/dos.png"
+    },
+    {
+        "id":"3-a",
+        "enunciado":"¿Cuántos cuadriláteros se pueden formar que tengan sus vértices en los puntos de la figura? ",
+        "opciones":{
+            "A":"$6$",
+            "B":"$12$",
+            "C":"$36$",
+            "D":"$18$",
+            "E":"$24$"
+        },
+        "imagen":"img/tres.png"
+    },
+    {
+        "id":"4-a",
+        "enunciado":" Los triángulos congruentes ABC y DCE son isósceles. Si AB=5 cm y el perímetro de toda la figura es 42 cm, ¿cuánto mide BC?",
+        "opciones":{
+            "A":"$4$",
+            "B":"$6$",
+            "C":"$8$",
+            "D":"$9$",
+            "E":"$10$"
+        },
+        "imagen":"img/cuatro.png"
+    },
+    {
+        "id":"5-b",
+        "enunciado":"La señora Eva tiene una mesa con la forma y dimensiones mostradas en la figura: Para que se conserve mejor va a colocarle un recubrimiento de vidrio en la superficie, ¿qué cantidad de vidrio, en metros cuadrados, usará para cubrir la mesa? Considere pi como 3.14. ",
+        "opciones":{
+            "A":"$7.57$",
+            "B":"$9.14$",
+            "C":"$12.28$",
+            "D":"$15.36$",
+            "E":"$18.56$"
+        },
+        "imagen":"img/cinco.png"
+    },
+    {
+        "id":"6-a",
+        "enunciado":"En una calle hay 100 casas. Si se van a fabricar los números para poner en todas las casas, que se van a numerar del 1 al 100. ¿Cuántos nueves se necesitan? ",
+        "opciones":{
+            "A":"$20$",
+            "B":"$19$",
+            "C":"$15$",
+            "D":"$10$",
+            "E":"$9$"
+        }
+    },
+    {
+        "id":"7-b",
+        "enunciado":" El automóvil de Jorge consume 12 L de gasolina en 132 km. Si en el tanque hay 5 L, ¿cuántos kilómetros puede recorrer su automóvil?",
+        "opciones":{
+            "A":"$65$",
+            "B":"$55$",
+            "C":"$50$",
+            "D":"$60$",
+            "E":"$45$"
+        }
+    },
+    {
+        "id":"8-a",
+        "enunciado":" En una sala de cine con cupo para 160 personas se registra la asistencia del público a una película. La sala se encuentra llena. La gráfica muestra la relación de adultos y menores de edad en la sala. Si hay 18 niñas por cada 12 niños presentes, ¿cuántas niñas hay en toda la sala?",
+        "opciones":{
+            "A":"$72$",
+            "B":"$60$",
+            "C":"$48$",
+            "D":"$40$",
+            "E":"$12$"
+        }
+    },
+    {
+        "id":"9-b",
+        "enunciado":" En una planta armadora de autos hay tres líneas de producción: la línea A arma 4 autos en 12 horas, la B arma 3 autos en 6 horas y la C arma el triple de la línea A en 24 horas. ¿Cuántos autos completos se arman en un turno de 6 horas?",
+        "opciones":{
+            "A":"$3$",
+            "B":"$4$",
+            "C":"$5$",
+            "D":"$6$",
+            "E":"$8$"
+        }
+    },
+    {
+        "id":"10-a",
+        "enunciado":" Tres ferrocarriles pasan por una estación de vía múltiple con los siguientes intervalos: uno cada 6 minutos, otro cada 9 minutos y el tercero cada 15 minutos. Si a las 16 horas pasan simultáneamente, ¿a qué hora pasarán de nuevo los tres trenes al mismo tiempo?",
+        "opciones":{
+            "A":"$16:45$",
+            "B":"$17:15$",
+            "C":"$17:00$",
+            "D":"$17:30$",
+            "E":"Ninguna"
+        }
+    },
+    {
+        "id":"11-a",
+        "enunciado":" ¿Cuál es el resultado de la siguiente operación? $$\\sqrt{(2015+2015)+(2015-2015)+(2015\\cdot 2015)+(2015/2015)}$$",
+        "opciones":{
+            "A":"$1000$",
+            "B":"$0$",
+            "C":"$2015$",
+            "D":"$2016$",
+            "E":"$1$"
+        }
+    },
+    {
+        "id":"12-b",
+        "enunciado":"Si se leen las afirmaciones en las opciones de izquierda a derecha, ¿cuál es la primera que es cierta?",
+        "opciones":{
+            "A":"(c) es cierta",
+            "B":"(a) es cierta",
+            "C":"(e) es falsa",
+            "D":"(b) es falsa",
+            "E":"$1+1=2$"
+        }
+    },
+    {
+        "id":"13-b",
+        "enunciado":"Dentro de un cuadrado de lado 2 se trazaron semicírculos (con $3$ de los lados como diámetros) y se sombreó como se muestra en la figura. ¿Cuál es el área sombreada?",
+        "opciones":{
+            "A":"$2$",
+            "B":"$4$",
+            "C":"$1$",
+            "D":"$2.5$",
+            "E":"$1.85$"
+        },
+        "imagen":"img/sombra.png"
+    },
+    {
+        "id":"14-b",
+        "enunciado":"Considera la siguiente lista de números 289, 49, 25, 121. De los números: 19, 36, 244, 169, 144, ¿cuál puede pertenecer a la lista?",
+        "opciones":{
+            "A":"$244$",
+            "B":"$169$",
+            "C":"$144$",
+            "D":"$36$",
+            "E":"$19$"
+        }
+    },
+    {
+        "id":"15-b",
+        "enunciado":"Se tienen 12 kg de carne de res, 18 kg de carne de cerdo y 30 kg de carne de pollo, que se desean empacar en bolsas con igual peso y con la máxima cantidad de carne posible. ¿Cuál es el peso, en kilogramos, de cada bolsa?",
+        "opciones":{
+            "A":"$2$",
+            "B":"$3$",
+            "C":"$6$",
+            "D":"$5$",
+            "E":"Ninguna de las anteriores"
+        }
+    },
+    {
+        "id":"16-b",
+        "enunciado":"El maestro de matemáticas compró 54 paletas y repartió una a cada uno de sus alumnos. Si a las niñas les tocó la tercera parte de las paletas y al maestro le sobraron 5, ¿cuántos niños hay en el grupo?",
+        "opciones":{
+            "A":"$18$",
+            "B":"$27$",
+            "C":"$31$",
+            "D":"$32$",
+            "E":"$41$"
+        }
+    },
+    {
+        "id":"17-b",
+        "enunciado":"El producto de tres enteros positivos consecutivos es igual a $120$. El números más chico es igual a:",
+        "opciones":{
+            "A":"$4$",
+            "B":"$9$",
+            "C":"$5$",
+            "D":"$6$",
+            "E":"$7$"
+        }
+    },
+    {
+        "id":"18-b",
+        "enunciado":"Si $p,q$ son números primos que satisfacen $p^2 = 45 + q^2$, el valor de $p+q$ es:",
+        "opciones":{
+            "A":"$2$",
+            "B":"$5$",
+            "C":"$7$",
+            "D":"$6$",
+            "E":"$9$"
+        }
+    },
+    {
+        "id":"19-b",
+        "enunciad o":"Max le preguntó a sus cinco amigos que cuántos de ellos habían estudiado para el examen de Matemáticas. Octavio dijo que ninguno. Gabriela dijo que solamente uno. Sunya dijo que exactamente dos. Marco dijo que exactamente tres y Claudia dijo que exactamente cuatro. Max sabe que los que no estudiaron estan diciendo mentiras, y que aquellos que estudiaron están diciendo la verdad. ¿Cuántos de los amigos de Max estudiaron para el examen? ",
+        "opciones":{
+            "A":"$2$",
+            "B":"$1$",
+            "C":"$4$",
+            "D":"$3$",
+            "E":"$5$"
+        }
+    },
+    {
+        "id":"20-b",
+        "enunciado":"Si $2^{2x}+2^x=20$. El valor de $x$ es:",
+        "opciones":{
+            "A":"$1$",
+            "B":"$2$",
+            "C":"$3$",
+            "D":"$4$",
+            "E":"$5$"
+        }
+    },
+    {
+        "id":"21-b",
+        "enunciado":"Una botella pequeña de shampoo contiene $35$ mililítros de shampoo, mientras que una botella grande contiene $500$ mililítros. Jasmine quiere comprar el mínimo número necesario de botellas pequeñas para llenar totalmente una botella grande. ¿Cuántas botellas debe comprar?",
+        "opciones":{
+            "A":"$12$",
+            "B":"$16$",
+            "C":"$14$",
+            "D":"$15$",
+            "E":"$13$"
+        }
+    },
+    {
+        "id":"22-b",
+        "enunciado":"Tres números primos $p,q,r$ satisfacen que $p<q<r$ y $p+q=r$. El único valor que puede tomar $p$ es:",
+        "opciones":{
+            "A":"$2$",
+            "B":"$6$",
+            "C":"$3$",
+            "D":"$5$",
+            "E":"$7$"
+        }
+    },
+    {
+        "id":"23-b",
+        "enunciado":"Varios piratas se repartieron un cofre con monedas de oro de manera que a cada uno le tocara misma cantidad. Si hubiera cuatro piratas menos, a cada persona le habría tocado 10 monedas más. Si hubiera 50 monedas menos, a cada persona le hubieran tocado 5 monead menos que en el reparto original. ¿Cuántas monedas se repartieron en total? ",
+        "opciones":{
+            "A":"$80$",
+            "B":"$100$",
+            "C":"$120$",
+            "D":"$150$",
+            "E":"$250$"
+        }
+    },
+    {
+        "id":"24-b",
+        "enunciado":"En la figura hay $3$ círculos concéntricos y dos diamétros perpendiculares. Si las tres áreas sombreadas son iguales y el radio del círculo menor es 1,¿cuál es el producto de los tres radios? ",
+        "opciones":{
+            "A":"$2$",
+            "B":"$4$",
+            "C":"$6$",
+            "D":"$\\sqrt{6}$",
+            "E":"$1$"
+        },
+        "imagen":"img/circulos.png"
+    },
+    {
+        "id":"25-b",
+        "enunciado":"Un cuadrado de papel se dobló hasta colocar una de sus esquinas exactamente en el centro, como se muestra en la figura. Con el doblez se formó un pentágono irregular, las áreas del pentágono y del cuadrado son dos números que son enteros consecutivos. ¿Cuál es el área del cuadrado? ",
+        "opciones":{
+            "A":"$5$",
+            "B":"$6$",
+            "C":"$7$",
+            "D":"$8$",
+            "E":"Faltan datos"
+        },
+        "imagen":"img/pentagono.jpg"
+    },
+    {
+        "id":"26-b",
+        "enunciado":"Montserrat sumó las longitudes de tres lados de un rectángulo y obtuvo 44 cm. Isabela también sumó las longitudes de tres lados del mismo rectángulo pero ella obtuvo 40 cm. ¿Cuál es el perímetro del rectángulo? ",
+        "opciones":{
+            "A":"$56$",
+            "B":"$154$",
+            "C":"$160$",
+            "D":"$48$",
+            "E":"$52$"
+        }
+    },
+    {
+        "id":"27-b",
+        "enunciado":"Juan Pablo tiene tres dispensadores de dulces que dan un dulce a la vez. No puede ver lo que tienen adentro, pero sabe que uno contiene dulces de cereza, otro esta lleno con dulces de limón y otro tienen de los dos sabores. También sabe que todas las etiquetas de los dispensadores se cambiaron entre sí y todas quedaron equivocadas. ¿Cuál es la menor cantidad de dulces que puede sacar para reetiquetar los dispensadores correctamente?",
+        "opciones":{
+            "A":"$1$",
+            "B":"$2$",
+            "C":"$9$",
+            "D":"$3$",
+            "E":"$4$"
+        }
+    },
+    {
+        "id":"28-b",
+        "enunciado":"Hay $10$ tarjetas numeradas del 1$1$ al $10$. En la figura se muestran $5$ de las tarjetas. Las restantes se quieres aparear con las que se muestran, de manera que las sumas de las parejas sean $9,10,11,12,13$(sin repetir). ¿De cuántas maneras es posible hacer esto?",
+        "opciones":{
+            "A":"$1$",
+            "B":"$2$",
+            "C":"$3$",
+            "D":"$4$",
+            "E":"$5$"
+        },
+        "imagen":"img/tarjetas.jpg"
+    }
+];
